51ccecffacbc9151e79e8992c3055dc16a1f6a12
Nenad <galaxija.737@gmail.com>
2025-06-02 04:58:31 +0200

    Update preload.js
    bridge between Electron and browser

--- preload.js
+++ preload.js
@@ -1,4 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
+
 contextBridge.exposeInMainWorld('electronAPI', {
-  // IPC endpoints go here
+  openFolder: () => ipcRenderer.invoke('dialog:openFolder')
 });
